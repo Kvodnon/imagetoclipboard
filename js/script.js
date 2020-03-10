17cd038800804fc56879de5e320d6cfea4db7de6
Kirill Kiselev <skulpin@mail.ru>
2020-03-10 22:58:55 +0700

    changes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const container = document.querySelector('.hide'),
-    copyButton = document.getElementById('copy');
+    copyButton = document.getElementById('copy'),
+    totalImageWidth = document.getElementById('totalImageWidth');
 
   const canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
-  
+
+    window.canvas = canvas;
+    window.ctx = ctx;
+
   let images = [],
     promises = [];
 
   container.addEventListener('click', (event) => {
     const target = event.target;
-    
+
     if (!target.matches('img')) return;
-    
+
     target.parentNode.classList.add('hidden');
   });
 
@@ -28,28 +32,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
     joinImages();
   });
-  
-  
+
+
   const joinImages = () => {
     const remainingImages = document.querySelectorAll('.hide-on-click:not(.hidden) img');
 
-    for (const remainImage of remainingImages) {
-      const image = new Image();
-      image.crossOrigin = "Anonymous";
-      image.src = remainImage.src;
+    if (remainingImages.length) {
+      for (const remainImage of remainingImages) {
+        const image = new Image();
+        image.crossOrigin = "Anonymous";
+        image.src = remainImage.src;
 
-      images.push(image);
-      
-      promises.push(new Promise((resolve, reject) => {
-        image.addEventListener('load', (event) => {
-          setCanvasWidth(event);
-          resolve();
-        });
-      }));
+        images.push(image);
+
+        promises.push(new Promise((resolve, reject) => {
+          image.addEventListener('load', (event) => {
+            setCanvasWidth(event);
+            resolve();
+          });
+        }));
+      }
+
+      Promise.all(promises)
+        .then(renderImages);
+    } else {
+      alert('Нет картинок на выбор.');
     }
-
-    Promise.all(promises)
-      .then(renderImages);
   }
 
   const setCanvasWidth = (event) => {
@@ -58,20 +66,40 @@ document.addEventListener('DOMContentLoaded', function() {
     canvas.width += image.width;
     canvas.height = image.height > canvas.height ? image.height : canvas.height;
   };
-  
+
+  const copyCanvasToClipboard = () => {
+    canvas.toBlob(function (blob) {
+      const item = new ClipboardItem({
+        "image/png": blob
+      });
+      navigator.clipboard.write([item]);
+    });
+  };
+
   const renderImages = () => {
+    const totalImage = new Image();
     let imagePositionX = 0;
 
     for (const image of images) {
-      ctx.drawImage(image, imagePositionX, 0,  image.width, image.height);
+      ctx.drawImage(image, imagePositionX, 0, image.width, image.height);
       imagePositionX += image.width;
     }
+    
+    totalImage.src = canvas.toDataURL("image/png");
 
-    const url = canvas.toDataURL("image/png");
+    totalImage.addEventListener('load', () => {
+      let totalWidth = totalImageWidth.value;
 
-    canvas.toBlob(function(blob) { 
-      const item = new ClipboardItem({ "image/png": blob });
-      navigator.clipboard.write([item]); 
+      if (!totalWidth) {
+        totalWidth = totalImage.width;
+      }
+
+      canvas.width = totalWidth;
+      canvas.height = totalImage.height * (totalWidth / totalImage.width);
+      ctx.drawImage(totalImage, 0, 0, totalWidth, totalImage.height * (totalWidth / totalImage.width));
+      
+      copyCanvasToClipboard();
     });
+
   };
 });
